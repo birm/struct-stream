4ca9c79dee1d427fedd510160dc5f4e2ab75d7d2
birm <birm@gatech.edu>
2016-05-12 16:26:21 -0400

    add project description for pypi

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='StructStream',
     author_email='birm@rbirm.us',
     author='Ryan Birmingham',
+    description='Save stream xml or json information for easy usage, keep size under control, use key constraints, and summarize your data.',
     url="http://rbirm.us/struct-stream",
     version='0.2',
     packages=['sstream'],
